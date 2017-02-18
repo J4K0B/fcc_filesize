a57045709d204432a3a6a8556b3f6908d3d6565e
Jakob <jakob.tepe@gmail.com>
2017-02-18 21:55:05 +0000

    built main functionality

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
+var path = require("path");
+
+app.post('/', upload.single('datei'), function (req, res, next) {
+    console.log("file incoming");
+  res.send({
+      "size": req.file.size
+  });
+});
+
+app.get("/", function(req, res) {
+    console.log("someone visited us");
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen("8080", function(){
+    console.log("Listening on 8080");
+});
